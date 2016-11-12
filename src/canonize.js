32c9a3e5cef499a178f9b7e4396abcd3a3ffd3d7
Sypachev Igor <sypachev@global-arts.ru>
2016-11-13 04:20:39 +0700

    clear

--- src/canonize.js
+++
@@ -1,6 +0,0 @@
-export default function canonize(url){
-    const reg = new RegExp('@?(https?:)?(\/\/)?((.*?)[^\/]*\/)?([@a-zA-z0-9._]*)','i');
-    const username = url.match(reg);
-    username[5] = username[5].replace('@',"");
-    return '@' + username[5];
-}
